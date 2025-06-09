44ebb0f610a354d1ee31919a753ab009c532110f
Wilgem Mapalad <wilgemmapalad714@gmail.com>
2025-06-09 16:39:07 +0800

    ""

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.credentials)
     implementation("com.google.android.gms:play-services-auth:21.0.0")
     implementation(libs.googleid)
+    implementation(libs.play.services.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
