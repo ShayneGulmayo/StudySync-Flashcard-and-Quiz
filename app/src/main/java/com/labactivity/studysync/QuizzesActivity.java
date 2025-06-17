8195c8ef7ff153fc76e12dfd071366690d594ec0
Aaronne Christian <aaronne.delacruz13@gmail.com>
2025-06-17 21:57:44 +0800

    added files: -
    edited:
    - QuizzesActivity java code

--- app/src/main/java/com/labactivity/studysync/QuizzesActivity.java
+++ app/src/main/java/com/labactivity/studysync/QuizzesActivity.java
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class QuizzesActivity extends AppCompatActivity {
 
     private ImageView addQuizButton;
+    private ImageView backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +18,21 @@ public class QuizzesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quizzes);
 
         addQuizButton = findViewById(R.id.add_button);
+        backButton = findViewById(R.id.back_button);
 
         addQuizButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(QuizzesActivity.this, CreateQuizActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        // button back to Home
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(); //replace with an Intent to HomeActivity once created
             }
         });
     }
