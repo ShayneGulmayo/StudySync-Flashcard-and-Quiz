83a6437fde1c683bda52dde50d2f98e74d3e0119
Aaronne Christian <aaronne.delacruz13@gmail.com>
2025-06-17 21:50:46 +0800

    added files: -
    edited:
    - QuizViewActivity java code

--- app/src/main/java/com/labactivity/studysync/QuizViewActivity.java
+++ app/src/main/java/com/labactivity/studysync/QuizViewActivity.java
@@ -3,7 +3,28 @@ package com.labactivity.studysync;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
-public class QuizViewActivity {
+
+public class QuizViewActivity extends AppCompatActivity {
+
+    private ImageView back_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_viewer);
+
+        back_button = findViewById(R.id.back_button);
+
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(QuizViewActivity.this, QuizProgressActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
 }
