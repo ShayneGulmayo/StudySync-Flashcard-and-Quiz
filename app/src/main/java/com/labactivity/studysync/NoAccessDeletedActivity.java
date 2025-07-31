e67fd90ea014cce19be5c2c2b4e6d118d6931c68
Aaronne Christian <aaronne.delacruz13@gmail.com>
2025-07-31 18:15:52 +0800

    edited/improved: -
    added files:
    - NoAccessDeletedActivity.java
    
    current bugs:
    -
    
    restored features:
    -

---
+++ app/src/main/java/com/labactivity/studysync/NoAccessDeletedActivity.java
@@ -0,0 +1,28 @@
+package com.labactivity.studysync;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class NoAccessDeletedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_no_access_deleted);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        findViewById(R.id.back_button).setOnClickListener(v -> finish());
+    }
+
+}
